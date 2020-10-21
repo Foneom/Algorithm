44f4a0755ddf6f515f0a2624e5ef6900dee50e79
Foneom <mugoloka@gmail.com>
2020-10-21 23:24:23 +0300

    Mod HighArray.java

--- src/main/java/com/foneom/arrays/HighArray.java
+++ src/main/java/com/foneom/arrays/HighArray.java
@@ -1,4 +1,11 @@
 package com.foneom.arrays;
 
 public class HighArray {
+    private int[] arr;
+    private int nElem;
+
+    public HighArray(int maxSize) {
+        this.arr = new int[maxSize];
+        this.nElem = 0;
+    }
 }
