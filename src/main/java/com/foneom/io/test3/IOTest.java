4bf71b39d5549e3a9b53264bd744ce64c0e1242d
Foneom <mugoloka@gmail.com>
2020-12-09 02:28:03 +0300

    split

--- src/main/java/com/foneom/io/test3/IOTest.java
+++ src/main/java/com/foneom/io/test3/IOTest.java
@@ -4,18 +4,18 @@ import java.io.*;
 
 public class IOTest {
 
-    public static void fileWrite() {
-        try {
-            PrintWriter writer =
-                    new PrintWriter(new BufferedWriter(new FileWriter(new File("text.txt"), true)));
-            writer.println("Hello");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public static void fileWrite() {
+            try {
+                PrintWriter writer =
+                        new PrintWriter(new BufferedWriter(new FileWriter(new File("text.txt"), true)));
+                writer.println("Hello");
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void main(String[] args) {
+            fileWrite();
         }
     }
-
-    public static void main(String[] args) {
-        fileWrite();
-    }
-}
