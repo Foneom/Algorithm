f94e9e4a422a3ae5a367880109a468d50f7fa01a
Foneom <mugoloka@gmail.com>
2020-12-09 02:25:09 +0300

    add newClass

--- src/main/java/com/foneom/io/analizy/Analizy.java
+++ src/main/java/com/foneom/io/analizy/Analizy.java
@@ -6,35 +6,36 @@ import java.util.List;
 
 public class Analizy {
 
-    public static void unavailable() throws IOException {
-        List list = new ArrayList();
 
-            Reader fr = new FileReader(new File("src/main/java/com/foneom/io/analizy/source.csv"));
-         //   BufferedReader reader = new BufferedReader(fr);
-            Writer fw = new FileWriter(new File("src/main/java/com/foneom/io/analizy/target.csv"));
-           // BufferedWriter writer = new BufferedWriter(fw);
-            char[] buffer = new char[1];
-            int line = fr.read(buffer);
-            while (line != -1) {
-                fw.write(buffer, 0, line);
-                line = fr.read(buffer);
+    public static void unavailable(String source, String target) throws IOException {
+
+        StringBuilder stringForTime = new StringBuilder();
+        List<String> log = new ArrayList<>();
+        boolean marker = false;
+
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (!marker && (line.startsWith("400") || line.startsWith("500"))) {
+                    marker = true;
+                    stringForTime = new StringBuilder(line + " - ");
+                }
+                if (marker && line.startsWith("200")) {
+                    marker = false;
+                    stringForTime.append(line).append(System.lineSeparator());
+                    log.add(stringForTime.toString());
+                    stringForTime = new StringBuilder();
+                }
             }
-            fr.close();
-            fw.close();
-    }
-
-    public static void main(String[] args) throws IOException {
-        /**
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("src/main/java/com/foneom/io/analizy/unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            log.forEach(out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-         */
-        unavailable();
-}
 }
 
 
